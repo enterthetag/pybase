eba9afe788fea589a38eadc5e70b44d48102f35a
Víctor Muñoz <victorm@marshland.es>
2022-10-26 17:18:32 +0100

    Remove allow-unsafe flag from pip-compile.

--- {{cookiecutter.project_slug}}/scripts/gen_requirements.sh
+++ {{cookiecutter.project_slug}}/scripts/gen_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-pip-compile --no-header --generate-hashes --allow-unsafe --output-file requirements/requirements.txt
-pip-compile --no-header --generate-hashes --allow-unsafe --extra dev --output-file requirements/dev.txt
+pip-compile --no-header --generate-hashes --output-file requirements/requirements.txt
+pip-compile --no-header --generate-hashes --extra dev --output-file requirements/dev.txt
