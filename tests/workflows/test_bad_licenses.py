f3caa431e1ac93deef79d6bd5ce5e72ddf651c5d
Víctor Muñoz <victorm@marshland.es>
2022-10-31 10:56:40 +0000

    Add a test for a missing license whitelist.

--- tests/workflows/test_bad_licenses.py
+++ tests/workflows/test_bad_licenses.py
@@ -1,3 +1,4 @@
+import pathlib
 import subprocess
 
 import pytest
@@ -34,7 +35,6 @@ def test_bad_license_package(package):
         run_cmd("tox", "-e", "licenses", venv=package.venv)
 
     assert error.value.returncode == 1
-
     assert (
         b"license GNU General Public License v2 (GPLv2) "
         b"not in allow-only licenses was found for package pylint"
@@ -47,3 +47,24 @@ def test_bad_license_package(package):
     result = run_cmd("tox", "-e", "licenses", venv=package.venv)
 
     assert result.returncode == 0
+
+
+def test_missing_license_whitelist(package):
+    """
+    Workflow under test:
+
+    - Create a package, including dependencies with non-whitelisted licenses.
+    - Remove the license whitelist file.
+    - Run the license audit script, from tox, so only the install dependencies
+      are present. It should fail, reporting the lack of a whitelist file.
+    """
+
+    pathlib.Path(".license-whitelist").unlink()
+
+    with pytest.raises(subprocess.CalledProcessError) as error:
+        run_cmd("tox", "-e", "licenses", venv=package.venv)
+
+    assert error.value.returncode == 1
+    assert (
+        b"license-whitelist: License whitelist not found, or found empty"
+    ) in error.value.stdout
